6cf20a4be1ba33161322a4afc64fce7441fde0a1
Dorian <doriancursos@gmail.com>
2024-07-17 09:47:40 +0200

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const adviceIdElement = document.getElementById('advice-id');
+const adviceTextElement = document.getElementById('advice-text');
+const buttonGenerateElement = document.getElementById('button-generate');
+
+const printAdvide = (id, advice) => {
+  adviceIdElement.textContent = `ADVICE #${id}`;
+  adviceTextElement.textContent = advice;
+};
+
+const requestAdvice = async () => {
+  try {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    const id = data.slip.id;
+    const advice = data.slip.advice;
+    printAdvide(id, advice);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+requestAdvice();
+
+buttonGenerateElement.addEventListener('click', requestAdvice);
